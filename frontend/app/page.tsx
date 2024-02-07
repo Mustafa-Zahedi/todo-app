e8924636ae0ef2051aff1f42f5257144e304b023
unknown <malestani21@gmail.com>
2024-02-07 10:08:36 +0430

    frotend dir added

---
+++ frontend/app/page.tsx
@@ -0,0 +1,10 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      todo app
+      <h1>Hello Vercel</h1>
+    </main>
+  );
+}
